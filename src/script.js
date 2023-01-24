e08a15db71f6219c0f8ec94fe2d65a9359226bdf
rachelblackie <rachelannblackie@gmail.com>
2023-01-24 10:38:01 +0100

    work on language change button

--- src/script.js
+++ src/script.js
@@ -1,7 +1,8 @@
+
 function changeLanguage() {
   let spanishSubheading = document.querySelector("#hero-h2");
   spanishSubheading.innerHTML =
     "Programadora front-end, profesora de inglés, y correctora/editora";
 }
-let spanishButton = document.querySelector("#spanish");
-spanishButton.addEventListener("click", changeLanguage);
+
+document.getElementById("#spanish").onclick = function(changeLanguage);
