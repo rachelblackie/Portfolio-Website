cfab15a5ed662c53db31aefc1f41284084957b1a
rachelblackie <rachelannblackie@gmail.com>
2023-01-25 11:50:50 +0100

    work on translation

--- src/language.js
+++ src/language.js
@@ -2,7 +2,7 @@ function changeToSpanish(event) {
   let spanishSubheading = document.querySelector("#hero-h2");
   spanishSubheading.innerHTML =
     "Programadora front-end, profesora de inglés, y correctora/editora";
-  let spanishContentHeading = document.querySelector("content-heading");
+  let spanishContentHeading = document.querySelector("#content-heading");
   spanishContentHeading.innerHTML = "Proyectos Recientes";
 }
 let spanishButton = document.querySelector("#spanish");
