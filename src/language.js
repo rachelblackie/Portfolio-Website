0e60de5bc5486e6fa812b74ae98fc4b6f29408fc
rachelblackie <rachelannblackie@gmail.com>
2023-01-25 12:55:51 +0100

    work on translation

--- src/language.js
+++ src/language.js
@@ -6,6 +6,13 @@ function changeToSpanish(event) {
   spanishWorkButton.innerHTML = "Trabaja conmigo";
   let spanishContentHeading = document.querySelector("#content-heading");
   spanishContentHeading.innerHTML = "Proyectos Recientes";
+  let spanishCrochetWebsite = document.querySelector("#crochet-site");
+  spanishCrochetWebsite.innterHTML = "Sitio web de crochet";
+  let spanishCrochetParagraph = document.querySelector("#crochet-paragraph");
+  spanishCrochetParagraph.innerHTML =
+    "Este sitio web es mi primer proyecto de codificar. Lo creé cuando empecé a aprender HTML, CSS, y Javascript. Incluye información sobre crochet, enlaces a recursos relacionados, y unos elementos de Javascript interactivos. ";
+  let spanishCrochetButton = document.querySelector("#website-link-crochet");
+  spanishCrochetButton.innerHTML = "Míralo";
 }
 let spanishButton = document.querySelector("#spanish");
 spanishButton.addEventListener("click", changeToSpanish);
